7afd9ac0119493970e78374123958912a9ccb128
刘淼 <liumiao@cvte.com>
2018-09-25 11:34:19 +0800

    export the TimePickerStyle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { TimePicker3Render } from "./TimePicker/TimePicker3";
+import { TimePicker3Style } from "./TimePicker/TimePicker3/TimePicker3Style";
+import { TimePicker3Render } from "./TimePicker/TimePicker3";
+
+export { TimePicker3Render, TimePicker3Style };
